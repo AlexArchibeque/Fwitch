7f8390648a5b1a48c83c1da37f568ea5d102245d
AlexArchibeque <wonkman33@gmail.com>
2020-10-12 16:51:13 -0700

    Backend Auth

---
+++ frontend/fwitchIndex.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootEl = document.getElementById('root')
+    // const store = configureStore();
+
+    ReactDOM.render(<h1>React Is connected!</h1>,rootEl)
+
+})
