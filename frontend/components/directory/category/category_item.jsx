e1c59d4c3040a502d5867aac9ea6580c388070c9
AlexArchibeque <wonkman33@gmail.com>
2020-12-04 01:40:06 -0800

    hover effect

--- frontend/components/directory/category/category_item.jsx
+++ frontend/components/directory/category/category_item.jsx
@@ -6,7 +6,7 @@ class CategoryItem extends React.Component{
         const {category} = this.props;
         return(
             <div className="category-item">
-                <a href={`#/directory/${category.title}`}><img src={category.photoUrl} /></a>
+                <a href={`#/directory/${category.title}`} className="stream"><img src={category.photoUrl}  className="stream__thumbnail"/></a>
                 <h1 className="category-item-text">{category.title}</h1>
                 <p className="category-item-text">{category.description}</p>
             </div>
--- frontend/components/directory/clips/clips_item.jsx
+++ frontend/components/directory/clips/clips_item.jsx
@@ -6,31 +6,15 @@ class ClipItem extends React.Component {
         this.props = props
     }
 
-    // componentDidMount(){
-    //     if(this.props.channels[this.props.video.channel_id] === undefined){
-    //         this.props.getChannel(this.props.video.channel_id)
-    //     }
-    // }
-
-    // window.justChatting = "<%= image_url('Just Chatting.jpg') %>"
     render() {
         const {video, channels} = this.props;
-        // let channel = this.props.channels[video.channel_id] != undefined ? true : false
         return(
             <div className="video-container">
-                <a href={`#/videos/${video.id}`}>
-                    <video src={`${video.videoUrl}`} type="video/mp4"/>
+                <a href={`#/videos/${video.id}`} className="stream">
+                    <video src={`${video.videoUrl}`} 
+                    type="video/mp4" 
+                    className="video-container-stuff stream__thumbnail"/>
                 </a>
-                {/* {channel ?
-                <div className='video-right-side-container'> 
-                    <img src={channels[video.channel_id].channelPhoto} />
-                    <div className="video-inner-text-right-side">
-                        <p>{channels[video.channel_id].ownerName}</p>
-                    </div>
-                </div>
-                
-                :
-                <div/>} */}
                 <p>{video.description}</p>
             </div>
         )
