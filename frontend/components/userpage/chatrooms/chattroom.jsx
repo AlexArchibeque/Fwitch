e2d6bbf742b79409915c0e03afb6bf9145b2afa2
AlexArchibeque <wonkman33@gmail.com>
2020-12-04 00:13:49 -0800

    css stuff for chatroom

--- frontend/components/userpage/chatrooms/chattroom.jsx
+++ frontend/components/userpage/chatrooms/chattroom.jsx
@@ -6,7 +6,21 @@ class ChatRoom extends React.Component {
     render(){
         return(
             <div className="chatroom-outer-div">
-                Temp Text
+                <div className="top-of-chatroom">STREAM CHAT</div>
+
+                <div className="middle-of-chatroom">
+                    Chatroom
+                </div>
+
+                <form className="bottom-of-chatroom">
+                    <input className="chatroom-input" type="text" placeholder="Send a message"/>
+                    <div className="bottom-bottom-of-chatroom">
+                        <div>
+                        </div>
+
+                        <button className="chatroom-chat-button hover-button">Chat</button>
+                    </div>
+                </form>
             </div>
         )
     }
